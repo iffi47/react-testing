7be30d1d68c7d79d6d110994fd8a05e8c317f866
iffi47 <iftikharlion958@gmail.com>
2026-07-27 19:06:21 +0500

    Add mock for fetch in Async component tests to simulate API response

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,10 @@ import Async from "./Async";
 
 describe("Async Component",() => {
 test("renders host",async() => {
+  window.fetch= jest.fn();
+  window.fetch.mockResolvedValueOnce({
+    json:async () => [{id: "p1", title: "First Post"}]
+  })
   render(<Async/>);
   const listItemElements = await screen.findAllByRole("listitem",{exact: false});
   expect(listItemElements).not.toHaveLength(0);
